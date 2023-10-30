9c50b1bb12794c61d057bbf0874e40d7aea05f82
suhedayayla <suhedayayla1@gmail.com>
2023-10-30 23:33:14 +0300

    beforeNavigator

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import SignInScreen from './src/screens/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import ConfirmEmailScreen from './src/screens/ConfirmEmailScreen';
+import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
+import NewPasswordScreen from './src/screens/NewPasswordScreen';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App = () => {
+  return (
+    <SafeAreaView>  
+      <NewPasswordScreen/>   
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+root:{
+  flex:1,
+  backgroundColor:'#F9FBFC',
+}
+});
+
+export default App;
